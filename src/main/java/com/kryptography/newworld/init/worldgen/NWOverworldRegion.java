f90074145d156f5428c4a69fff76c094e2012b16
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2024-12-10 13:50:12 +0400

    Update biome gen yet again

--- src/main/java/com/kryptography/newworld/init/worldgen/NWOverworldRegion.java
+++ src/main/java/com/kryptography/newworld/init/worldgen/NWOverworldRegion.java
@@ -5,6 +5,8 @@ import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.*;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Climate;
 import terrablender.api.ParameterUtils;
 import terrablender.api.ParameterUtils.*;
 import terrablender.api.Region;
@@ -21,6 +23,14 @@ public class NWOverworldRegion extends Region {
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper) {
         VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
-        addBiomeSimilar(mapper, Biomes.MEADOW, NWBiomes.WOODED_MEADOW);
+        new ParameterUtils.ParameterPointListBuilder()
+                .temperature(Temperature.ICY, Temperature.COOL, Temperature.NEUTRAL, Temperature.FROZEN)
+                .humidity(Humidity.FULL_RANGE)
+                .continentalness(Continentalness.span(Continentalness.FAR_INLAND, Continentalness.FAR_INLAND), Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND))
+                .erosion(Erosion.EROSION_1, Erosion.EROSION_2, Erosion.EROSION_3)
+                .depth(Depth.SURFACE)
+                .weirdness(Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.PEAK_VARIANT, Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.PEAK_NORMAL, Weirdness.MID_SLICE_NORMAL_ASCENDING)
+                .build().forEach(point -> builder.add(point, NWBiomes.WOODED_MEADOW));
+        builder.build().forEach(mapper);
     }
 }
