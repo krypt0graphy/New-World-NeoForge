7e097606f64b4164dca845b86587e59b19e86b62
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2024-12-12 15:35:38 +0400

    Update worldgen... Why doesn't this ever work man

--- src/main/java/com/kryptography/newworld/init/worldgen/NWOverworldRegion.java
+++ src/main/java/com/kryptography/newworld/init/worldgen/NWOverworldRegion.java
@@ -4,7 +4,6 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.biome.*;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
 import terrablender.api.ParameterUtils;
@@ -26,10 +25,10 @@ public class NWOverworldRegion extends Region {
         new ParameterUtils.ParameterPointListBuilder()
                 .temperature(Temperature.ICY, Temperature.COOL, Temperature.NEUTRAL, Temperature.FROZEN)
                 .humidity(Humidity.FULL_RANGE)
-                .continentalness(Continentalness.span(Continentalness.FAR_INLAND, Continentalness.FAR_INLAND), Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND))
-                .erosion(Erosion.EROSION_1, Erosion.EROSION_2, Erosion.EROSION_3)
-                .depth(Depth.SURFACE)
-                .weirdness(Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.PEAK_VARIANT, Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.PEAK_NORMAL, Weirdness.MID_SLICE_NORMAL_ASCENDING)
+                .continentalness(Continentalness.FAR_INLAND)
+                .erosion(Erosion.EROSION_0, Erosion.EROSION_1, Erosion.EROSION_2, Erosion.EROSION_3)
+                .depth(Depth.SURFACE, Depth.FLOOR)
+                .weirdness(Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.PEAK_VARIANT, Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.MID_SLICE_NORMAL_ASCENDING,  Weirdness.PEAK_NORMAL)
                 .build().forEach(point -> builder.add(point, NWBiomes.WOODED_MEADOW));
         builder.build().forEach(mapper);
     }
