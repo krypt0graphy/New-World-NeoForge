d9577f1e0192390d6009545569d19da2fbd25ae7
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2024-12-21 20:07:06 +0400

    Change wooded meadow spawn rate

--- src/main/java/com/kryptography/newworld/init/worldgen/NWBiomePlacement.java
+++ src/main/java/com/kryptography/newworld/init/worldgen/NWBiomePlacement.java
@@ -6,7 +6,7 @@ import net.minecraft.world.level.biome.Biomes;
 public class NWBiomePlacement {
 
     public static void register() {
-        BiomePlacement.replaceOverworld(Biomes.MEADOW, NWBiomes.WOODED_MEADOW, 0.2);
+        BiomePlacement.replaceOverworld(Biomes.MEADOW, NWBiomes.WOODED_MEADOW, 0.3);
     }
 
 }
