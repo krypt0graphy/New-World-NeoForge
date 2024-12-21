3155976adff1c79d197e977577df83dc7d814627
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2024-12-22 00:37:37 +0400

    Fix problems with datagen

--- src/main/java/com/kryptography/newworld/init/data/NWData.java
+++ src/main/java/com/kryptography/newworld/init/data/NWData.java
@@ -56,6 +56,7 @@ public class NWData {
         gen.addProvider(event.includeServer(), blockTagsProvider);
         gen.addProvider(event.includeClient(), new NWItemTagsProvider(packOutput, lookup, blockTagsProvider.contentsGetter(), existingFileHelper));
         gen.addProvider(event.includeClient(), new NWItemModelProvider(packOutput, existingFileHelper));
+        gen.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(), List.of(new LootTableProvider.SubProviderEntry(NWChestLootProvider::new, LootContextParamSets.CHEST)), lookup));
         gen.addProvider(event.includeClient(), new NWBlockStateProvider(packOutput, existingFileHelper));
         gen.addProvider(event.includeServer(), new NWDataMapProvider(packOutput, lookup));
         gen.addProvider(event.includeServer(), new NWRecipeProvider(packOutput, lookup));
