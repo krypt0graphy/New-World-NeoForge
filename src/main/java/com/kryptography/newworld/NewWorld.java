574032552464589dd83b7e0654f1ac58dc478d8a
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2025-01-10 21:15:06 +0400

    Fix Server Crash

--- src/main/java/com/kryptography/newworld/NewWorld.java
+++ src/main/java/com/kryptography/newworld/NewWorld.java
@@ -1,23 +1,17 @@
 package com.kryptography.newworld;
 
-import com.kryptography.newworld.client.NWBoatRenderer;
 import com.kryptography.newworld.init.*;
 import com.kryptography.newworld.init.worldgen.NWOverworldRegion;
 import com.kryptography.newworld.init.worldgen.features.NWFeature;
 import com.kryptography.newworld.init.data.NWData;
 import com.kryptography.newworld.init.data.NWStats;
 import com.kryptography.newworld.init.data.loot.NWLootModifiers;
-import com.kryptography.newworld.init.data.woodset.FirBlockSet;
 import com.kryptography.newworld.init.worldgen.structure.NWStructureTypes;
-import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import terrablender.api.Regions;
@@ -52,15 +46,6 @@ public class NewWorld {
             NWData.vanillaInteractions();
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(NWBlocks.FIR_SAPLING.getId(), NWBlocks.POTTED_FIR_SAPLING);
             Regions.register(new NWOverworldRegion(id("overworld"), 5));
-        });
-    }
-
-    @SubscribeEvent
-    public static void clientSetup(FMLClientSetupEvent evt) {
-        evt.enqueueWork(() -> {
-            Sheets.addWoodType(FirBlockSet.FIR_WOOD_TYPE);
-            EntityRenderers.register(NWEntityTypes.FIR_BOAT.get(), pContext -> new NWBoatRenderer(pContext, false));
-            EntityRenderers.register(NWEntityTypes.FIR_CHEST_BOAT.get(), pContext -> new NWBoatRenderer(pContext, true));
         });
     }
 
--- src/main/java/com/kryptography/newworld/client/NWEventBusClientEvents.java
+++ src/main/java/com/kryptography/newworld/client/NWEventBusClientEvents.java
@@ -3,22 +3,26 @@ package com.kryptography.newworld.client;
 
 import com.kryptography.newworld.init.NWBlockEntityTypes;
 import com.kryptography.newworld.init.NWEntityTypes;
+import com.kryptography.newworld.init.data.woodset.FirBlockSet;
 import net.minecraft.client.model.BoatModel;
 import net.minecraft.client.model.ChestBoatModel;
+import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 
-@Mod.EventBusSubscriber(modid = "newworld", bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = "newworld", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class NWEventBusClientEvents {
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
+        Sheets.addWoodType(FirBlockSet.FIR_WOOD_TYPE);
         EntityRenderers.register(NWEntityTypes.FIR_BOAT.get(), pContext -> new NWBoatRenderer(pContext, false));
         EntityRenderers.register(NWEntityTypes.FIR_CHEST_BOAT.get(), pContext -> new NWBoatRenderer(pContext, true));
     }
