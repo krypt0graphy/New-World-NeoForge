ef30fe9f233615ef3231bf749e71e7c65880cb07
Krypto <114697909+krypt0graphy@users.noreply.github.com>
2025-02-06 14:37:58 +0400

    Fix missing recipe, update texture

--- src/main/java/com/kryptography/newworld/common/data/providers/NWRecipeProvider.java
+++ src/main/java/com/kryptography/newworld/common/data/providers/NWRecipeProvider.java
@@ -110,6 +110,16 @@ public class NWRecipeProvider extends RecipeProvider implements IConditionBuilde
                         NWItems.ANCIENT_MATTOCK.asItem())
                 .unlocks("has_mattock_crafting_template", has(NWItems.MATTOCK_CRAFTING_TEMPLATE))
                 .save(pRecipeOutput, NewWorld.id("ancient_mattock_smithing"));
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, NWItems.MATTOCK_CRAFTING_TEMPLATE.get(), 2)
+                .pattern("#T#")
+                .pattern("#C#")
+                .pattern("###")
+                .define('#', Items.DIAMOND)
+                .define('C', Items.COBBLED_DEEPSLATE)
+                .define('T', NWItems.MATTOCK_CRAFTING_TEMPLATE.get())
+                .unlockedBy("has_mattock_crafting_template", has(NWItems.MATTOCK_CRAFTING_TEMPLATE.get()))
+                .save(pRecipeOutput);
     }
 
     public void stoneSetRecipes(RecipeOutput pRecipeOutput, ItemLike base, ItemLike stairs, ItemLike slab, ItemLike wall) {
