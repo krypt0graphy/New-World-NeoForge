084963082338138010f89b67a3b57e75523750f7
Mohammad Merghani <114697909+krypt0graphy@users.noreply.github.com>
2026-08-02 22:30:13 +0400

    Fir fallen log

--- src/main/java/com/kryptography/newworld/common/worldgen/features/FallenLogFeature.java
+++ src/main/java/com/kryptography/newworld/common/worldgen/features/FallenLogFeature.java
@@ -5,49 +5,111 @@ import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 
-public class FallenLogFeature extends Feature<NoneFeatureConfiguration>{
+public class FallenLogFeature extends Feature<NoneFeatureConfiguration> {
 
+	private static final int MIN_LOG_LENGTH = 5;
+	private static final int MAX_LOG_LENGTH = 9;
+	private static final int MAX_GROUND_GAP = 2;
+	private static final float MUSHROOM_CHANCE = 0.1F;
 
-	public FallenLogFeature(Codec<NoneFeatureConfiguration> pCodec) {
-		super(pCodec);
+	public FallenLogFeature(Codec<NoneFeatureConfiguration> codec) {
+		super(codec);
 	}
 
 	@Override
 	public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context) {
-		Direction direction = Direction.Plane.HORIZONTAL.getRandomDirection(context.random());
 		WorldGenLevel level = context.level();
-		BlockPos pos = context.origin();
+		BlockPos origin = context.origin();
+		RandomSource random = context.random();
 
-		int logLength = 2 + context.random().nextInt(2, 4);
-		if(level.canSeeSky(pos) || level.getBlockState(pos.below()).isSolid() && !level.getBlockState(pos.below()).is(Blocks.WATER) && !level.getBlockState(pos.below()).is(Blocks.LAVA)) {
-				for (int i = 0; i < logLength; i++) {
-					placeLog(level, pos, direction, i);
-
-				}
-				return true;
+		if (!isValidGround(level, origin)) {
+			return false;
 		}
-		return false;
+
+		level.setBlock(origin, NWBlocks.FIR_LOG.get().defaultBlockState().setValue(RotatedPillarBlock.AXIS, Direction.Axis.Y), 2);
+
+		Direction direction = Direction.Plane.HORIZONTAL.getRandomDirection(random);
+		int gap = 2 + random.nextInt(2);
+		BlockPos.MutableBlockPos pos = origin.relative(direction, gap).mutable();
+
+		setGroundHeightForFallenLogStartPos(level, pos);
+
+		int length = MIN_LOG_LENGTH + random.nextInt(MAX_LOG_LENGTH - MIN_LOG_LENGTH + 1);
+
+		if (canPlaceEntireFallenLog(level, pos, direction, length)) {
+			placeFallenLog(level, random, pos, direction, length);
+		}
+
+		return true;
 	}
 
-	public void placeLog(WorldGenLevel level, BlockPos pos, Direction direction, int currentLoop) {
-		BlockPos currentPos = pos.relative(direction, currentLoop);
-		if(level.getBlockState(currentPos.below()).isSolid() && level.getBlockState(currentPos).is(BlockTags.REPLACEABLE)) {
-			level.setBlock(currentPos, NWBlocks.FIR_LOG.get().defaultBlockState().setValue(RotatedPillarBlock.AXIS, direction.getAxis()), 2);
-		} else {
-			boolean foundValid = false;
-			for (int j = -6; j < 3; j++) {
-				if (level.getBlockState(currentPos.below(j)).isSolid() && level.getBlockState(currentPos.below(j - 1)).is(BlockTags.REPLACEABLE) && !foundValid) {
-					level.setBlock(currentPos.below(j - 1), NWBlocks.FIR_LOG.get().defaultBlockState().setValue(RotatedPillarBlock.AXIS, direction.getAxis()), 2);
-					foundValid = true;
+	private void setGroundHeightForFallenLogStartPos(WorldGenLevel level, BlockPos.MutableBlockPos pos) {
+		pos.move(Direction.UP, 1);
+
+		for (int i = 0; i < 6; i++) {
+			BlockState below = level.getBlockState(pos.below());
+			BlockState at = level.getBlockState(pos);
+			if (below.isSolid() && at.is(BlockTags.REPLACEABLE)) {
+				return;
+			}
+			pos.move(Direction.DOWN);
+		}
+	}
+
+	private boolean canPlaceEntireFallenLog(WorldGenLevel level, BlockPos.MutableBlockPos pos, Direction direction, int length) {
+		BlockPos.MutableBlockPos checkPos = pos.mutable();
+		int consecutiveGaps = 0;
+
+		for (int i = 0; i < length; i++) {
+			BlockState at = level.getBlockState(checkPos);
+			if (!at.is(BlockTags.REPLACEABLE)) {
+				return false;
+			}
+
+			BlockState below = level.getBlockState(checkPos.below());
+			if (!below.isSolid() || below.is(Blocks.WATER) || below.is(Blocks.LAVA)) {
+				if (++consecutiveGaps > MAX_GROUND_GAP) {
+					return false;
+				}
+			} else {
+				consecutiveGaps = 0;
+			}
+
+			checkPos.move(direction);
+		}
+
+		return true;
+	}
+
+	private void placeFallenLog(WorldGenLevel level, RandomSource random, BlockPos.MutableBlockPos pos, Direction direction, int length) {
+		BlockState logState = NWBlocks.FIR_LOG.get().defaultBlockState().setValue(RotatedPillarBlock.AXIS, direction.getAxis());
+
+		for (int i = 0; i < length; i++) {
+			level.setBlock(pos, logState, 2);
+
+			if (random.nextFloat() < MUSHROOM_CHANCE) {
+				BlockPos abovePos = pos.above();
+				if (level.getBlockState(abovePos).is(BlockTags.REPLACEABLE)) {
+					level.setBlock(abovePos, Blocks.RED_MUSHROOM.defaultBlockState(), 2);
 				}
 			}
+
+			pos.move(direction);
 		}
+	}
+
+	private boolean isValidGround(WorldGenLevel level, BlockPos pos) {
+		BlockState below = level.getBlockState(pos.below());
+		BlockState at = level.getBlockState(pos);
+		return level.canSeeSky(pos) && below.isSolid() && !below.is(Blocks.WATER) && !below.is(Blocks.LAVA) && at.is(BlockTags.REPLACEABLE);
 	}
 }
